24f564c3d06eca75277364d4991408ef165ccea9
TryCatchB <pozdrakovzena@gmail.com>
2025-02-01 14:10:43 +0300

    Update files

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,9 +1,25 @@
-import React, { FC } from 'react';
-import buildFirstLevel from './utils/buildFirstLevel';
-import styles from './Menu.module.css';
+'use client';
 
-const Menu: FC = async (): Promise<JSX.Element> => {
-  return <nav className={styles.menu}>{buildFirstLevel()}</nav>;
+import React, { FC, useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import styles from './Menu.module.css';
+import FirstLevel from './utils/FirstLevel';
+
+const Menu: FC = () => {
+  const pathName = usePathname();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null; // Предотвращаем рендеринг на сервере
+
+  return (
+    <nav className={styles.menu}>
+      <FirstLevel pathName={pathName} />
+    </nav>
+  );
 };
 
 export default Menu;
